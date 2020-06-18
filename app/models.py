4fd36ead34eb71254dd6cfd7b7e3eea094fff68c
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-18 11:18:26 +0530

    Added url link to copy

--- app/models.py
+++ app/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class ShortUrl(models.Model):
 	short_url = models.CharField(max_length=20)
-	long_url = models.URLField("URL", unique=True)
+	long_url = models.URLField(max_length=200)
+
+	def __str__(self):
+		return f"{self.long_url} => {self.short_url}"
